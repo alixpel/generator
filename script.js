7ea64d084f8ebbc11193dc7425a93ed6bfadfacb
Alix Pelletier <alixpelletierpro@gmail.com>
2021-01-16 23:57:37 +0100

    clean code

--- script.js
+++ script.js
@@ -10,16 +10,6 @@ fetch('https://aws.random.cat/meow')
   container.appendChild(image);
 });
 
-// fetch('https://aws.random.cat/meow')
-// .then(function(body) { // body est la réponse de la requête effectuée
-//   return body.json(); // Evalue la réponse de l'objet
-// })
-// .then(function(json) {
-//   var image = document.getElementById('test');
-//   image.src = json.file;
-// });
-
-
 fetch('https://api.kanye.rest/')
 .then(function(body) { // body est la réponse de la requête effectuée
   return body.json(); // Evalue la réponse de l'objet
